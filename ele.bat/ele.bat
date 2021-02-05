d3fbb09d8e09ebde8f5e9f044e2f96d58e2a23ee
Justin B <46439389+JustinBrow@users.noreply.github.com>
2021-02-05 16:36:11 -0500

    Update ele.bat

--- ele.bat/ele.bat
+++ ele.bat/ele.bat
@@ -1,7 +1,27 @@
-@echo off
+@ECHO OFF
 
 :: ele.bat
 :-------------------------------------
+fsutil dirty query %systemdrive% >nul
+IF NOT '%errorlevel%' == 0 (
+   ECHO Requesting administrative privileges...
+   GOTO getPrivileges
+) ELSE (
+   GOTO gotPrivileges
+)
+
+:getPrivileges
+   ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getPrivileges.vbs"
+   SET params = %*:"=""
+   ECHO UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getPrivileges.vbs"
+   
+   "%SystemRoot%\System32\WScript.exe" "%temp%\getPrivileges.vbs"
+   DEL "%temp%\getPrivileges.vbs"
+   EXIT /B
+
+:gotPrivileges
+   pushd "%CD%"
+   CD /D "%~dp0"
 
 :--------------------------------------
 
