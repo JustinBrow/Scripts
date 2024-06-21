b4b43e1389f6a066e51418091394ec3837bdb9d3
Justin B <46439389+JustinBrow@users.noreply.github.com>
2024-06-21 11:05:24 -0400

    Update wtsapi.psm1

--- wtsapi/wtsapi.psm1
+++ wtsapi/wtsapi.psm1
@@ -1,3 +1,6 @@
+$OldEAP = $ErrorActionPreference
+$ErrorActionPreference = 'Stop'
+
 $moduleDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
 if (Test-Path ("$moduleDir\wtsapi.dll"))
@@ -14,15 +17,49 @@ if (Test-Path ("$moduleDir\wtsapi.dll"))
    if ($assemblyVersion -ne $sourceVersion)
    {
       Add-Type -TypeDefinition (Get-Content ("$moduleDir\wtsapi.cs") | Out-String) -OutputType Library -OutputAssembly ("$moduleDir\wtsapi.dll")
-      Import-Module ("$moduleDir\wtsapi.dll")
+      try
+      {
+         Import-Module ("$moduleDir\wtsapi.dll")
+         Write-Host "WtsApi version $assemblyVersion loaded"
+      }
+      catch
+      {
+         $_
+      }
    }
    else
    {
-      Import-Module ("$moduleDir\wtsapi.dll")
+      try
+      {
+         Import-Module ("$moduleDir\wtsapi.dll")
+         Write-Host "WtsApi version $assemblyVersion loaded"
+      }
+      catch
+      {
+         $_
+      }
    }
 }
 else
 {
+   Get-Content ("$moduleDir\wtsapi.cs") -ReadCount 1 | ForEach-Object {
+      $regex = [Regex]::Match($_, 'Version\("(?<Version>\d\.\d\.\d\.\d)"\)')
+      if ($regex.Success -eq $true)
+      {
+         $sourceVersion = [Version]::Parse($regex.Groups['Version'].Value)
+         return
+      }
+   }
    Add-Type -TypeDefinition (Get-Content ("$moduleDir\wtsapi.cs") | Out-String) -OutputType Library -OutputAssembly ("$moduleDir\wtsapi.dll")
-   Import-Module ("$moduleDir\wtsapi.dll")
+   try
+   {
+      Import-Module ("$moduleDir\wtsapi.dll")
+      Write-Host "WtsApi version $sourceVersion loaded"
+   }
+   catch
+   {
+      $_
+   }
 }
+
+$ErrorActionPreference = $OldEAP
