cc3121ea2c62551945ba3bd10bfe05e108615b2b
Justin <46439389+JustinBrow@users.noreply.github.com>
2024-07-23 13:59:14 -0400

    Create QBBackup.ps1

---
+++ QuickBooks Backup/QBBackup.ps1
@@ -0,0 +1,67 @@
+$datetimeFormat = 'MM/dd/yyyy hh:mm tt'
+$logFile = 'C:\logs\QBBackup.log'
+$errorLog = 'C:\logs\QBBackup_error.log'
+
+if (Test-Path $logFile)
+{
+   Remove-Item -LiteralPath $logFile
+}
+if (Test-Path $errorLog)
+{
+   Remove-Item -LiteralPath $errorLog
+}
+
+try
+{
+   [Xml]$qbbackup = Get-Content 'C:\ProgramData\Intuit\QuickBooks Enterprise Solutions 24.0\qbbackup.sys'
+}
+catch
+{
+   $Error[0] | Out-File -LiteralPath $errorLog -Append
+   $Error.Clear()
+}
+
+if ($qbbackup -is [Xml])
+{
+   ForEach ($file in $qbbackup.backupdata.file)
+   {
+      if ($file.scheduled.Count -ge 2)
+      {
+         "$($file.name) has multiple backup schedules. To use this script please schedule only 1 backup" | Out-File -LiteralPath $errorLog -Append
+      }
+
+      if ($file.scheduled)
+      {
+         if (Test-Path $file.name)
+         {
+            $params = @{
+               FilePath = 'C:\Program Files\Intuit\QuickBooks Enterprise Solutions 24.0\AutoBackupEXE.exe'
+               ArgumentList = "/F$($file.name) /S /I$($file.scheduled.id)"
+               Wait = $true
+               PassThru = $true
+            }
+
+            "$($file.name)  started at $(Get-Date -Format $datetimeFormat)" | Out-File -LiteralPath $logFile -Append
+
+            try
+            {
+               $process = Start-Process @params
+            }
+            catch
+            {
+               $Error[0] | Out-File -LiteralPath $errorLog -Append
+               $Error.Clear()
+            }
+
+            "$($file.name) finished at $($process.ExitTime.ToString($datetimeFormat)) with code $($process.ExitCode)" | Out-File -LiteralPath $logFile -Append
+
+            if ($process.ExitCode -ne 2)
+            {
+               "Exit code was not 2 at $(Get-Date -Format $datetimeFormat) for $($file.name)" | Out-File -LiteralPath $errorLog -Append
+            }
+
+            Start-Sleep -Seconds 1
+         }
+      }
+   }
+}
