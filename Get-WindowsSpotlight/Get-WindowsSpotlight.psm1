6810d2bf82a2c498848b4068e002dc727fb3e8fa
Justin <46439389+JustinBrow@users.noreply.github.com>
2024-10-26 23:33:38 -0400

    Update Get-WindowsSpotlight.psm1

--- Get-WindowsSpotlight/Get-WindowsSpotlight.psm1
+++ Get-WindowsSpotlight/Get-WindowsSpotlight.psm1
@@ -5,44 +5,76 @@ function Get-WindowsSpotlight
       [ValidateScript({Test-Path -Path $_})]
       [string]$Destination = "$env:USERPROFILE\Downloads"
    )
-
+   
    if ($Source)
    {
-      $Files = Get-ChildItem -Path $source
+      if (Test-Path -Path $Source)
+      {
+         $Files = Get-ChildItem -Path $source
+      }
+      else
+      {
+         throw
+      }
    }
    else
    {
       $Files = Get-ChildItem -Path "$env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
    }
-
+   
    $To = [IO.Path]::Combine($Destination, 'Windows Spotlight')
-   if (-Not (Test-Path -Path $To))
+   if (-not (Test-Path -Path $To))
    {
-      New-Item -Type Directory -Path $To | Select-Object -ExpandProperty FullName
-   } 
-   foreach ($File in $Files)
-   {
-      $arrBytes = Get-Content -Path $File.FullName -Encoding Byte -TotalCount 4
-      $strBytes = [System.BitConverter]::ToString($arrBytes) -replace '-'
-      switch -Regex ($strBytes)
+      try
       {
-         '^FFD8....$'
+         New-Item -Type Directory -Path $To | Select-Object -ExpandProperty FullName
+      }
+      catch
+      {
+         throw
+      }
+   }
+   
+   if ($Files.Count -ge 1)
+   {
+      foreach ($File in $Files)
+      {
+         $arrBytes = Get-Content -Path $File.FullName -Encoding Byte -TotalCount 4
+         $strBytes = [BitConverter]::ToString($arrBytes) -replace '-'
+         switch -Regex ($strBytes)
          {
-            $FileName = $File.Name + '.jpg'
-            $Target = [IO.Path]::Combine($To, $FileName)
-            Copy-Item -Path $File.FullName -Destination $Target
-            continue
-         }
-         '^89504E47$'
-         {
-            $FileName = $File.Name + '.png'
-            $Target = [IO.Path]::Combine($To, $FileName)
-            Copy-Item -Path $File.FullName -Destination $Target
-            continue
-         }
-         default
-         {
-            'Unknown Magic Number: ' + $strBytes
+            '^FFD8\w{4}$'
+            {
+               $FileName = $File.Name + '.jpg'
+               $Target = [IO.Path]::Combine($To, $FileName)
+               try
+               {
+                  Copy-Item -Path $File.FullName -Destination $Target
+               }
+               catch
+               {
+                  throw
+               }
+               continue
+            }
+            '^89504E47$'
+            {
+               $FileName = $File.Name + '.png'
+               $Target = [IO.Path]::Combine($To, $FileName)
+               try
+               {
+                  Copy-Item -Path $File.FullName -Destination $Target
+               }
+               catch
+               {
+                  throw
+               }
+               continue
+            }
+            default
+            {
+               'Unknown Magic Number: ' + $strBytes
+            }
          }
       }
    }
