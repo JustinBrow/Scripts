2542dd10ba08947c5773bdb78c766fc69babba94
Justin <46439389+JustinBrow@users.noreply.github.com>
2025-12-31 15:25:35 -0500

    Update ddns.sh

--- ddns.sh
+++ ddns.sh
@@ -4,7 +4,7 @@ password='password'
 ip6=$(wget -q 'https://ifconfig.me/ip' -O -)
 echo "$(date) DDNS: Current IPv6 address is $ip6" >> /var/log/messages
 if [[ $ip =~ '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))' ]]; then
-   aaaa=$(nslookup $hostname 'ns1.he.net' | awk '/^Address: / { print $2 }')
+   aaaa=$(nslookup -type=AAAA $hostname 'ns1.he.net' | awk '/^Address: / { print $2 }')
    echo "$(date) DDNS: Current dynamic DNS IPv6 address is $aaaa" >> /var/log/messages
    if [ $aaaa != $ip6 ]; then
       echo "$(date) DDNS: Current dynamic DNS IPv6 address does not match current IPv6 address" >> /var/log/messages
