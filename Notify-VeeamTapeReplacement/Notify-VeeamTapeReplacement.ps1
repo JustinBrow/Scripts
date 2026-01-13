80242089fa8c01dcd09a226184f88d2b1ff74183
Justin <46439389+JustinBrow@users.noreply.github.com>
2026-01-13 14:13:02 -0500

    Update Notify-VeeamTapeReplacement.ps1

--- Notify-VeeamTapeReplacement/Notify-VeeamTapeReplacement.ps1
+++ Notify-VeeamTapeReplacement/Notify-VeeamTapeReplacement.ps1
@@ -1,5 +1,11 @@
+$TapeJob = 'SOBR_TAPE_MONTHLY'
+$From = 'no-reply@example.com'
+$To = 'me@example.com'
+$Cc = 'supervisor@example.com'
+$MailServer = 'mail.example.com'
+
 $job = $null
-$job = (Get-VBRTapeBackupSession -Job (Get-VBRTapeJob -Name 'SOBR_TAPE_MONTHLY')).Log | where Title -match 'Unloading|Exporting' | where Time -GT (Get-Date -Day 1)
+$job = (Get-VBRTapeBackupSession -Job (Get-VBRTapeJob -Name $TapeJob)).Log | where Title -match 'Unloading|Exporting' | where Time -GT (Get-Date -Day 1)
 if ($job -ne $null)
 {
    $tapes = Get-VBRTapeMedium | where IsExpired -eq $true | select Barcode, ExpirationDate, MediaSet, LastWriteTime, Name, IsRetired, IsExpired, ProtectedBySoftware | select -Property *, @{label='LastWriteTimeInDays'; expression = {([datetime]::UtcNow - $_.LastWriteTime).Days}} | sort LastWriteTimeInDays
@@ -7,5 +13,5 @@ if ($job -ne $null)
    $html = $tape | ConvertTo-Html -PreContent ($job | select Title, Time, Status | ConvertTo-Html -Fragment) -PostContent ($tapes | ConvertTo-Html -Fragment)
    $tape = Get-VBRTapeMedium -Name $tape.Name
    Disable-VBRTapeProtection -Medium $tape
-   Send-MailMessage -Body ($html -join '') -BodyAsHtml -Cc 'supervisor@example.com' -From 'no-reply@example.com' -SmtpServer 'mail.example.com' -Subject 'Veeam' -To 'me@example.com'
+   Send-MailMessage -Body ($html -join '') -BodyAsHtml -From $From -To $To -Cc $Cc -Subject 'Veeam Tape Status' -SmtpServer $MailServer
 }
